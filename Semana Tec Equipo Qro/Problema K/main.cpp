ec294b4fc6d138080252fd557a2e817bf80de032
eduardogil11 <70300067+eduardogil11@users.noreply.github.com>
2021-10-28 20:28:36 -0500

    Problema K

--- Semana Tec Equipo Qro/Problema K/main.cpp
+++ Semana Tec Equipo Qro/Problema K/main.cpp
@@ -5,19 +5,16 @@
 // =========================================================
 
 #include<bits/stdc++.h>
-#define m 100000+100
-typedef long long ll;
 
 using namespace std;
 
-// Variables que utilizaremos
-const ll x = 8e18;
-const int y = 1e5 + 10;
-ll dp[3][y];
-int a[y];
-
 int main() {
-    ll n, p, q, r;
+    // Variables que utilizaremos
+    const long long x = 8e18;
+    const int y = 1e5 + 10;
+    long long dp[3][y];
+    int a[y];
+    long long n, p, q, r;
 
     // Damos los 4 enteros
     cin >> n >> p >> q >> r;
@@ -28,19 +25,18 @@ int main() {
     }
 
     // Utilizaremos programación dinamica y tendremos varias posiblidades
-    dp[0][0] =- x;
-    dp[1][0] =- x;
-    dp[2][0] =- x;
+    dp[0][0] =-x;
+    dp[1][0] =-x;
+    dp[2][0] =-x;
 
     // Usamos max para comparar y devolver el mayor número
     for(int i = 1; i <= n; i++) {
         dp[0][i] = max(dp[0][i - 1], p * a[i]);
-        dp[1][i] = max(dp[1][i - 1], dp[0][i] + q * a[i]);
-        dp[2][i] = max(dp[2][i - 1], dp[1][i] + r * a[i]);
+        dp[1][i] = max(dp[1][i - 1], dp[0][i] + q*a[i]);
+        dp[2][i] = max(dp[2][i - 1], dp[1][i] + r*a[i]);
     } 
 
     // Imprimimos el resultado
-    int ans = dp[2][n];
-    cout << ans << endl;
+    cout << dp[2][n] << endl;
     return 0;
 }
